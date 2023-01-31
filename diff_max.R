8ba2ca4ac1cf5242cb30667f749dc5d7a06a9ed3
Loreqa <120091915+Loreqa@users.noreply.github.com>
2023-01-31 13:11:58 +0100

    Add files via upload

---
+++ diff_max.R
@@ -0,0 +1,31 @@
+diff_max <- function(df_teljes) {
+  columns = colnames(df_teljes) 
+  diff_df = data.frame(matrix(nrow = 1, ncol = length(columns))) 
+  colnames(diff_df) = columns
+  
+  for(i in 1:ncol(diff_df)) {       
+    diff_df[1,i] <- which.max(diff(df_teljes[,i]))
+  }
+  
+  diff_df <- t(diff_df[,-1])
+  hist(diff_df)
+  diff_df <- as.data.frame(diff_df)
+  colnames(diff_df) <- "date"
+  diff_df$cell <- rownames(diff_df)
+  
+  beadas <- which.max(table(diff_df$date))
+  diff_df$same <- NA
+  diff_df$difference <- NA
+  
+  for (i in 1:nrow(diff_df)) {
+    if (diff_df[i,1] == beadas) {
+      diff_df[i,3] <- TRUE
+      diff_df[i,4] <- diff_df[i,1] - beadas
+    } else {
+      diff_df[i,3] <- FALSE
+      diff_df[i,4] <- diff_df[i,1] - beadas
+    }
+  }
+  
+  return(beadas)
+}
