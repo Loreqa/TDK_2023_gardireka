1f3edba5dfb0d509c250952f6bb729d5260da600
Loreqa <120091915+Loreqa@users.noreply.github.com>
2023-02-17 10:08:21 +0100

    Add files via upload

---
+++ training_atalakito.R
@@ -0,0 +1,23 @@
+training_atalakito <- function(df_eredeti, df_spontan) {
+  
+  df_eredeti[(nrow(df_eredeti)+1),] <- c("spontan", t(df_spontan))
+  colnames(df_eredeti) <- c("Frame", paste0("ROI", 1:(ncol(df_eredeti)-1)))
+  df_eredeti <- as.data.frame(t(df_eredeti))
+  colnames(df_eredeti) <- df_eredeti[1,]
+  df_eredeti <- df_eredeti[-1, ] 
+  
+  sornev <- rownames(df_eredeti)
+  oszlopnev <- colnames(df_eredeti)
+  
+  df_eredeti <- lapply(df_eredeti, as.numeric)
+  df_eredeti$spontan <- as.factor(df_eredeti$spontan)
+  df_eredeti <- as.data.frame(df_eredeti)
+  
+  colnames(df_eredeti) <- oszlopnev
+  rownames(df_eredeti) <- sornev
+  
+  colnames(df_eredeti) <- paste("t_", oszlopnev, sep = "")
+  rownames(df_eredeti) <- sornev
+  colnames(df_eredeti)[ncol(df_eredeti)] <- "spontan"
+  
+}
